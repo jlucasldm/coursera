319cac3a5814839a64b6d3bc92bae5c35ae0c58b
jlucasldm <49415623+jlucasldm@users.noreply.github.com>
2020-07-06 16:39:05 -0300

    cleaning folders and updating php assignment3

---
+++ webAppForEverybody/buildingWebAppPhp/assignment3/code/index.php
@@ -0,0 +1,43 @@
+<title>João Lucas PHP</title>
+
+<h1>João Lucas PHP</h1>
+<p>The SHA256 hash of "João Lucas" is
+<?php
+/*
+ Getting Started with PHP
+Your application will be a single index.php file with some 
+elements in HTML and some elements in PHP. This also verifies 
+that you can do basic HTML and know how to use the 'pre' tag 
+as well as understand how to switch between PHP and HTML when 
+writing a PHP script. Make sure to save your screen shots JPG 
+or PNG files and keep them less than 1MB. You must include the 
+URL bar in all of your screen shots and the software must be 
+running in your PHP server (i.e. file:// urls will not be 
+accepted)
+
+Please do your reviews of the other students submissions 
+carefully. The actual points you award are less important 
+than useful comments about what might be wrong and need fixing. 
+You cannot re-submit your assignment unless the instructor 
+allows you to resubmit.
+
+Assignment specification: https://www.wa4e.com/assn/php/
+Sample solution: https://www.wa4e.com/solutions/php/
+*/
+print hash('sha256', 'João Lucas')
+
+?>
+</p>
+<pre>
+ASCII ART:
+
+    *********
+         **
+         **
+         **
+    *    **
+    *******
+</pre>
+<a href="./check.php">Click here to check the error setting</a>
+<br>
+<a href="./fail.php">Click here to cause a traceback</a>
